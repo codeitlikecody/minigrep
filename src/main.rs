ecfcacaec0f07c265d61c834d2086f65cb77d855
Chris Cody <3807221+codeitlikecody@users.noreply.github.com>
2024-11-04 23:52:09 +1100

    implemented basic file input completed tutorial to 12-02

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +8,7 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {query} in {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Oops! Couldn't read file!");
+    println!("File contents:\n{contents}");
 }
